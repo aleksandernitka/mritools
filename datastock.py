afe7cdff41e933ad39a6cbe67af17bc8e7b22970
Aleksander Nitka <aleksander.nitka@uj.edu.pl>
2022-10-14 12:01:56 +0200

    data stock script

---
+++ datastock.py
@@ -0,0 +1,50 @@
+import argparse
+from os.path import exists, join
+import pandas as pd
+
+# Input args parser
+parser = argparse.ArgumentParser(description='Script for taking stock of the data. Inside there directories which should be looked into are listed.')
+parser.add_argument('-o', '--out', help='Output file. Defaults to data_stock.csv.', default='data_stock.csv')
+parser.add_argument('-i', '--ids', help='Input ID list file (default is valid_subjects.csv).', default='valid_subjects.csv')
+parser.add_argument('-r', '--return', help='Return all IDs or just those with something missing; default: all', default='all', choices=['all', 'missing', 'with_everything'])
+parser = parser.parse_args()
+
+# Paths to check
+paths = {
+    'raw': '/mnt/nasips/COST_mri/rawdata/',
+    'hmri':'/mnt/nasips/COST_mri/derivatives/hmri/',
+    'freesurfer': '/mnt/nasips/COST_mri/derivatives/freesurfer/',
+    'dwi': '/mnt/nasips/COST_mri/derivatives/dwi/'
+}
+
+# Load the Ids for which we need to do the check
+df_subs = pd.read_csv(parser.ids)
+ids = df_subs.ID.values
+print(f'{len(ids)} valid subjects loaded from {parser.ids}')
+
+# start the main df for stock keeping
+df = pd.DataFrame(ID=ids, columns=['ID'])
+
+for d in paths.keys():
+    # Add the directory name to the df columns
+    df[d] = None
+    for i, sub in enumerate(ids):
+        # Fully qualified path
+        fqp = join(paths[d], sub)
+        # directory should be there with a fqp
+        df.loc[i, d] = exists(fqp)
+
+if 'all' in parser.return:
+    df.to_csv(parser.out, index=False)
+
+elif 'missing' in parser.return:
+    df = df[df[['hmri', 'dwi', 'freesurfer']] != True].reset_index(drop=True)
+    df.to_csv(join(parser.out.split('.csv')[0]+'_missing.csv'), index=False)
+
+elif 'with_everything' in parser.return:
+    df = df[df[['hmri', 'dwi', 'freesurfer']] == True].reset_index(drop=True)
+    df.to_csv(join(parser.out.split('.csv')[0]+'_with_everything.csv'), index=False)
+
+else:
+    print('What have you done???')
+    exit()
