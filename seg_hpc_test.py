7799b62f1ed0418ed4cd9ed9a59adab5825c08e3
Aleksander Nitka <aleksander.nitka@uj.edu.pl>
2022-11-24 09:43:05 +0100

    clean

--- seg_hpc_test.py
+++
@@ -1,8 +0,0 @@
-from fstools import seg
-
-segment = seg(subjects_dir='/Volumes/clab/aleksander/tmp_fshpc/subs/',\
-        stats_output_dir='/Volumes/clab/aleksander/tmp_fshpc/stats/',\
-        analysis_id='testPy2',\
-        pd_images_dir='/Volumes/clab/aleksander/tmp_fshpc/pds/')
-
-segment.run_hpc_list(['sub-', 'sub-'])
