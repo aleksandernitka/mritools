463246ecf870bcddd66cc61a707d6c3752161463
Aleksander Nitka <aleksander.nitka@uj.edu.pl>
2023-01-10 20:26:08 +0100

    fixed thn seg

--- fstools.py
+++ fstools.py
@@ -2,11 +2,28 @@
 
 class seg:
     """
-    Runs the segmentation algorithms on a set of freesurfer data. Extracts stats to a separate directory for easier access.
+    Runs the segmentation algorithms on a set of freesurfer data.
     The script will check if the subject has been processed before, and if so it will skip it.
-    The script will also check if the subject has a PD image, and if not it will skip it.
+    The script will also check if the subject has a PD image, and if not it will skip it, in case when such image is required. 
+
+    Parameters
+    ----------
+    subjects_dir    : str   location of freesurfer subjects_dir
+    analysis_id     : str   name of the analysis
+    pd_images_dir   : str   location of the PD images
+    threads         : int   number of threads to use
+    telegram        : bool  send notifications to telegram
+    skip_existing   : bool  skip subjects that have been processed before
+    hpc             : bool  run HPC segmentation
+    thn             : bool  run Thalamic Nuclei segmentation
+    bss             : bool  run Brainstem segmentation
+    hts             : bool  run Hypothalamus segmentation
+    scl             : bool  run Subcortical Limbic segmentation
+
+
     """
-    def __init__(self, subjects_dir, analysis_id, pd_images_dir, threads=40, telegram=True, skip_existing=True):
+    def __init__(self, subjects_dir, analysis_id, pd_images_dir, threads=40, telegram=True, skip_existing=True, \
+        hpc=False, thn=False, bss=False, hts=False, scl=False):
 
         from os.path import exists, expanduser
         from time import perf_counter as ptime
@@ -24,6 +41,12 @@ class seg:
         self.dt = datetime
         self.timings = [] # keep the durations for each subject
         self.errlog = f'{self.analysis_id}_errlog.txt'
+
+        self.hpc = hpc # HPC segmentation
+        self.thn = thn # Thalamic Nuclei segmentation
+        self.bss = bss # Brainstem segmentation
+        self.hts = hts # Hypothalamus segmentation
+        self.scl = scl # Subcortical Limbic segmentation
 
         if self.telegram:
             try:
@@ -298,7 +321,7 @@ class seg:
                     continue
             
             print(f'\nRunning THN segmentation on {subject} ({i+1}/{len(self.subjects)})\n')
-            self.run_hpc_sub(subject, print_count=False)
+            self.run_thn_sub(subject, print_count=False)
 
             # print progress info
             self.progress_info(i)
@@ -306,3 +329,69 @@ class seg:
         print(f'Finished THN segmentation on {len(self.subjects)} subjects')
         if self.telegram:
             self.tgsend(f'Finished THN segmentation on {len(self.subjects)} subjects')
+
+    def run_segmentation(self, subject_id, print_count=True):
+
+        # Runs a single subject through all specified segmentations
+        # TODO - once done, remove hpc and thn sub runs
+
+        import subprocess as sp
+        from os.path import join, exists
+        from os import listdir as ls
+
+        # Check subject id
+        if not subject_id.startswith('sub-'):
+            subject_id = f'sub-{subject_id}'
+
+        # Check if the subject exists
+        if not exists(join(self.subjects_dir, subject_id)):
+            self.log_error(subject_id, 'Subject dir does not exist')
+            print(f"Subject {subject_id} does not exist")
+            return None
+        
+        # Check if the subject has a PD image
+        # list all PD images in mpm dir
+        pd_images = [f for f in ls(join(self.pd_images_dir, subject_id, 'Results')) if f.endswith('PD.nii')]
+        if len(pd_images) == 0:
+            self.log_error(subject_id, 'PD image does not exist')
+            print(f"Subject {subject_id} does not have a PD image")
+            return None
+        elif len(pd_images) > 1:
+            self.log_error(subject_id, 'Multiple PD images')
+            print(f"Subject {subject_id} has multiple PD images")
+            return None
+        # otherwise we will have exactly one image -> pd_images[0]
+
+        if self.hpc:
+            process = sp.run(f'export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS={self.threads}; segmentHA_T2.sh {subject_id} \
+            {join(self.pd_images_dir, subject_id, "Results", pd_images[0])} \
+            {self.analysis_id} \
+            1 \
+            {self.subjects_dir}', shell=True, capture_output=True)
+
+        if self.thn:
+            process = sp.run(f'export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS={self.threads}; \
+            segmentThalamicNuclei.sh {subject_id} \
+            {self.subjects_dir} \
+            {join(self.pd_images_dir, subject_id, "Results", pd_images[0])} \
+            {self.analysis_id} \
+            t2', shell=True, capture_output=True)
+
+        # Above sp.run will suppress the output from the subprocess, capture it and print it if there is an error
+        # If all is well we can access the log file in the subject's directory, but errors may get lost. 
+
+        if process.returncode != 0:
+            self.log_error(subject_id, 'THN Segmentation failed')
+            print(f"Subject {subject_id} THN segmentation failed: {process.stderr.decode('utf-8')}")
+            # Also save it for later
+            with open(f'{subject_id}_thn_seg_err.txt', 'w') as f:
+                f.write(process.stderr.decode('utf-8'))
+                f.close()
+            return None
+        
+        # end time
+        tend = self.ptime()
+        self.timings.append(tend-tstart)
+        print(f'Finished THN segmentation on {subject_id}, it took {(tend-tstart)/60} minutes')
+
+        return None
